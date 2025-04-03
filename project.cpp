145a307cb236aec68b71c9e592fd506b00590e68
Slrm1 <168603144+Slrm1@users.noreply.github.com>
2025-04-02 22:48:09 -0400

    code commit

--- project.cpp
+++ project.cpp
@@ -3,3 +3,8 @@
 #include <cstdio>
 #include <cstdlib>
 #define SIZE 1000000000
+#define INT_OPERATION_TIME 100
+#define FLOAT_OPERATION_TIME 100
+#define MEMORY_OPERATION_TIME 100
+#define HARD_DRIVE_OPERATION_1_TIME 250
+#define HARD_DRIVE_OPERATION_2_TIME 10
